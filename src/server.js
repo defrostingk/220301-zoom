1fa3e7fdeb166c990fd297bc7b9d86fea57df3e3
Haedong Kim <haedong1111@gmail.com>
2022-03-01 23:22:33 +0900

    livereload

--- src/server.js
+++ src/server.js
@@ -1,11 +1,24 @@
 import express from "express";
+import livereload from "livereload";
+import connectLivereload from "connect-livereload";
+
+const liveReloadServer = livereload.createServer({
+  exts: ["pug", "js", "scss"],
+});
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
 
 const app = express();
+app.use(connectLivereload());
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const PORT = 3000;
 const handelListen = () => console.log(`Listening on http://localhost:${PORT}`);

079cada4b5f02333452e0ed2c87c4586ee05e003
Haedong Kim <haedong1111@gmail.com>
2022-03-04 15:05:09 +0900

    #2.3 Recap

--- src/server.js
+++ src/server.js
@@ -26,10 +26,10 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  socket.on("enter_room", (msg, done) => {
-    console.log(msg);
+  socket.on("enter_room", (roomName, done) => {
+    console.log(roomName);
     setTimeout(() => {
-      done();
+      done("asdasd");
     }, 10000);
   });
 });

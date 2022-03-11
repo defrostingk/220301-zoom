cad1c928cf3856b7f6b0068ff74f27b599c73219
Haedong Kim <haedong1111@gmail.com>
2022-03-11 22:52:50 +0900

    #3.9 STUN

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -16,6 +16,13 @@ let cameraOff = false;
 let roomName;
 let myPeerConnection;
 
+initButton();
+
+function initButton() {
+  muteBtn.innerText = muted ? "Unmute" : "Mute";
+  cameraBtn.innerText = cameraOff ? "Turn Camera On" : "Turn Camera Off";
+}
+
 async function getDevices() {
   await getCameras();
   await getMikes();
@@ -189,7 +196,19 @@ socket.on("ice", (ice) => {
 // RTC Code
 
 function makeConnection() {
-  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          "stun:stun.l.google.com:19302",
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+          "stun:stun3.l.google.com:19302",
+          "stun:stun4.l.google.com:19302",
+        ],
+      },
+    ],
+  });
   myPeerConnection.addEventListener("icecandidate", handleIce);
   myPeerConnection.addEventListener("addstream", handleAddStream);
   myStream

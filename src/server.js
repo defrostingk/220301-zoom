78210d3e437290fb3de9f6d8f8070f384c83db9f
Haedong Kim <haedong1111@gmail.com>
2022-03-21 15:35:33 +0900

    Heroku

--- src/server.js
+++ src/server.js
@@ -2,24 +2,12 @@ import http from 'http';
 import { Server } from 'socket.io';
 import { instrument } from '@socket.io/admin-ui';
 import express from 'express';
-import livereload from 'livereload';
-import connectLivereload from 'connect-livereload';
-
-const liveReloadServer = livereload.createServer({
-  exts: ['pug', 'js', 'scss'],
-});
-liveReloadServer.server.once('connection', () => {
-  setTimeout(() => {
-    liveReloadServer.refresh('/');
-  }, 100);
-});
 
 const app = express();
-app.use(connectLivereload());
 
 app.set('view engine', 'pug');
-app.set('views', __dirname + '/views');
-app.use('/public', express.static(__dirname + '/public'));
+app.set('views', process.cwd() + '/views');
+app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', (req, res) => res.render('home'));
 app.get('/*', (req, res) => res.redirect('/'));
 

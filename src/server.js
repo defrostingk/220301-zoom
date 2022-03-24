e737d82eb012d2a79491d9c187582d5fc4dcaadf
Haedong Kim <haedong1111@gmail.com>
2022-03-24 23:41:51 +0900

    Terminate livereload for heroku

--- src/server.js
+++ src/server.js
@@ -3,23 +3,7 @@ import { Server } from 'socket.io';
 import { instrument } from '@socket.io/admin-ui';
 import express from 'express';
 
-// For Live Reload Server
-import livereload from 'livereload';
-import connectLivereload from 'connect-livereload';
-
-const liveReloadServer = livereload.createServer({
-  exts: ['pug', 'js', 'css'],
-});
-liveReloadServer.server.once('connection', () => {
-  setTimeout(() => {
-    liveReloadServer.refresh('/');
-  }, 100);
-});
-
 const app = express();
-
-// live reload
-app.use(connectLivereload());
 
 app.set('view engine', 'pug');
 app.set('views', process.cwd() + '/src/views');
